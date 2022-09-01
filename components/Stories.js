d9b240c5102ef9df56dd6b67da20d09232076139
Leo <leoc2628@gmail.com>
2022-09-01 17:30:22 -0400

    populate stories icons

--- components/Stories.js
+++ components/Stories.js
@@ -1,10 +1,13 @@
 import { faker } from '@faker-js/faker';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Story from './Story';
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+
   function createRandomUser() {
     return {
-    //   userId: faker.datatype.uuid(),
+      //   userId: faker.datatype.uuid(),
       username: faker.internet.userName(),
       name: faker.name.firstName(),
       phone: faker.phone.number(),
@@ -20,11 +23,20 @@ function Stories() {
       id: i,
     }));
 
-    console.log(suggestions);
-
+    setSuggestions(suggestions);
   }, []);
 
-  return <div></div>;
+  return (
+    <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll'>
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Stories;
