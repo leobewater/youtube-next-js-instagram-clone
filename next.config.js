4e010dcac0dc26bea6cee6667bc2cbe6975f4913
Leo <leoc2628@gmail.com>
2022-09-01 07:56:43 -0400

    add image domain name to config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // add own custom domain names for image src
+  images: {
+    domains: [
+      "links.papareact.com",
+      "firebasestorage.googleapis.com"
+    ]
+  }
 }
 
 module.exports = nextConfig
