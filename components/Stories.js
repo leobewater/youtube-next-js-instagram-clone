ef103c32652a3dc446fb61ac6526a77ceec662c7
Leo <leoc2628@gmail.com>
2022-09-01 08:59:53 -0400

    use faker to generate faker users

--- components/Stories.js
+++ components/Stories.js
@@ -1,11 +1,30 @@
-function Stories() {
+import { faker } from '@faker-js/faker';
+import { useEffect } from 'react';
 
-    
-  return (
-    <div>
-        
-    </div>
-  )
+function Stories() {
+  function createRandomUser() {
+    return {
+    //   userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      name: faker.name.firstName(),
+      phone: faker.phone.number(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      registeredAt: faker.date.past(),
+    };
+  }
+
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...createRandomUser(),
+      id: i,
+    }));
+
+    console.log(suggestions);
+
+  }, []);
+
+  return <div></div>;
 }
 
-export default Stories
+export default Stories;
