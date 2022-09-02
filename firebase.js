bebdf15acf72622737313c23339df25b43cb42bd
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-02 10:19:29 -0400

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBliBSr6DANdSk-0i88kI6zUR_-kD_emXE",
+  authDomain: "nextjs-instagram-d49b8.firebaseapp.com",
+  projectId: "nextjs-instagram-d49b8",
+  storageBucket: "nextjs-instagram-d49b8.appspot.com",
+  messagingSenderId: "1089239801474",
+  appId: "1:1089239801474:web:4ed9e3d44fe629a8cc1e9b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
