192493ef69c05dcdcb76c016f09622fdcf12756f
Leo <leoc2628@gmail.com>
2022-09-02 16:43:52 -0400

    add styling to signin page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,17 +1,38 @@
 import { getProviders, signIn as SignIntoProvider } from 'next-auth/react';
+import Header from '../../components/Header';
 
 function signInPage({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img
+          className="w-80"
+          src="https://links.papareact.com/ocw"
+          alt="logo"
+        />
+        <p className="font-xs italic">
+          This is not a REAL app, it is built for educational purposes only
+        </p>
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: '/' })
+                }
+                className="p-3 bg-blue-500 rounded-lg text-white"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
-  )
+  );
 }
 
 // server side rendering
@@ -20,9 +41,9 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      providers
-    }
-  }
+      providers,
+    },
+  };
 }
 
-export default signInPage
+export default signInPage;
