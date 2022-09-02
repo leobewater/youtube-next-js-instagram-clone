4b4003b0f2554be47ab38eec339fbf818ad6ae0c
Leo <leoc2628@gmail.com>
2022-09-02 16:25:56 -0400

    add sessionprovider to app

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,11 +1,11 @@
-import { getProviders, signIn } from 'next-auth/react';
+import { getProviders, signIn as SignIntoProvider } from 'next-auth/react';
 
 function signInPage({ providers }) {
   return (
     <>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
+          <button onClick={() => SignIntoProvider(provider.id)}>
             Sign in with {provider.name}
           </button>
         </div>
