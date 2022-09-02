648d5d86e87d974daaabad3d2ae19dbdf3d4f570
Leo <leoc2628@gmail.com>
2022-09-02 16:43:38 -0400

    modify google provider response

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,6 +12,18 @@ export default NextAuth({
   ],
   // define own signin and signout pages
   pages: {
-    signIn: "/auth/signin"
-  }
-})
+    signIn: '/auth/signin',
+  },
+  // modify the google provider response
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split(' ')
+        .join('')
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      
+      return session;
+    },
+  },
+});
