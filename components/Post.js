3410da1b16b621abac7d819318d863dc3a7bd976
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-07 14:23:25 -0400

    hide post buttons and comment fields for guest

--- components/Post.js
+++ components/Post.js
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/react';
 import {
     BookmarkIcon,
     ChatBubbleBottomCenterIcon,
@@ -12,6 +13,8 @@ import {
 } from '@heroicons/react/24/solid';
 
 function Post({ id, username, userImg, img, caption }) {
+    const { data: session } = useSession();
+
     return (
         <div className='bg-white my-7 border rounded-sm'>
             {/* header */}
@@ -25,14 +28,17 @@ function Post({ id, username, userImg, img, caption }) {
             <img className="object-cover w-full" src={img} alt="Image" />
 
             {/* buttons */}
-            <div className='flex justify-between px-4 pt-4'>
-                <div className='flex space-x-4'>
-                    <HeartIcon className='btn' />
-                    <ChatBubbleBottomCenterIcon className='btn' />
-                    <PaperAirplaneIcon className='btn -rotate-90' />
+            {session && (
+                <div className='flex justify-between px-4 pt-4'>
+                    <div className='flex space-x-4'>
+                        <HeartIcon className='btn' />
+                        <ChatBubbleBottomCenterIcon className='btn' />
+                        <PaperAirplaneIcon className='btn -rotate-90' />
+                    </div>
+                    <BookmarkIcon className='btn' />
                 </div>
-                <BookmarkIcon className='btn' />
-            </div>
+            )}
+
 
             {/* caption */}
             <p className='p-5 truncate'>
@@ -42,11 +48,13 @@ function Post({ id, username, userImg, img, caption }) {
             {/* comments */}
 
             {/* input box */}
-            <form className='flex items-center p-4'>
-                <FaceSmileIcon className='h-7' />
-                <input type="text" placeholder="Add a comment..." className='border-none flex-1 focus:ring-0 outline-none'/>
-                <button className='font-semibold text-blue-400'>Post</button>
-            </form>
+            {session && (
+                <form className='flex items-center p-4'>
+                    <FaceSmileIcon className='h-7' />
+                    <input type="text" placeholder="Add a comment..." className='border-none flex-1 focus:ring-0 outline-none' />
+                    <button className='font-semibold text-blue-400'>Post</button>
+                </form>
+            )}
         </div>
 
     )
