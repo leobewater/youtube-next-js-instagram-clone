3ae65b98999627bce37138e13f499d2eadb0dd56
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-07 14:21:18 -0400

    populate posts from firebase

--- components/Posts.js
+++ components/Posts.js
@@ -1,35 +1,30 @@
-import postcss from "postcss";
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
+import { useEffect, useState } from "react";
 import Post from "./Post"
-
-const posts = [
-  {
-    id: '123',
-    username: 'ccc',
-    userImg: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/899.jpg',
-    img: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/899.jpg',
-    caption: 'Subscribe and destroy the like button for the YT algorithm!'
-  },
-  {
-    id: '333',
-    username: 'aaa',
-    userImg: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/899.jpg',
-    img: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/899.jpg',
-    caption: 'Subscribe and destroy the like button for the YT algorithm!'
-  },
-  {
-    id: '444',
-    username: 'bbb',
-    userImg: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/899.jpg',
-    img: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/899.jpg',
-    caption: 'Subscribe and destroy the like button for the YT algorithm!'
-  },
-];
+import { db } from '../firebase';
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // snapshot provides a realtime update
+    return onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+      setPosts(snapshot.docs);
+    });
+  }, []);
+
+  // console.log(posts);
+
   return (
     <div>
       {posts.map(post => (
-        <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption} />
+        <Post
+          key={post.id} 
+          id={post.id} 
+          username={post.data().username} 
+          userImg={post.data().profile} 
+          img={post.data().image} 
+          caption={post.data().caption} />
       ))}
     </div>
   )
